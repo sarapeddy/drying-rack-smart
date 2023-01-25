2041f41aa1a61f8c13bc1415487a3c32e997e8a8
Sara Pederzoli <sarapeddy06@gmail.com>
2023-01-25 17:08:45 +0100

    sistemati i vari file

--- arduino-sensors-drs/arduino-sensors-drs.ino
+++ arduino-sensors-drs/arduino-sensors-drs.ino
@@ -25,17 +25,17 @@ void loop(){
     /*DHT11: AIR HUMIDITY AND TEMPERATURE*/
     int chk = DHT.read11(DHT11_PIN);
     
-    Serial.print("Temperature = ");
+    /*Serial.print("Temperature = ");
     Serial.println(DHT.temperature);
     Serial.print("Humidity = ");
-    Serial.println(DHT.humidity);
+    Serial.println(DHT.humidity);*/
     
   
     /*CAPACITIVE SOIL MOISTURE SENSOR*/
     int moisture_soil_perc = analogRead(MOISTURESENSOR)*100/1023;
-    Serial.print("Soil moisture: ");
+    /*Serial.print("Soil moisture: ");
     Serial.print(moisture_soil_perc);
-    Serial.println("%");
+    Serial.println("%");*/
 
     data = { DHT.humidity, DHT.temperature, moisture_soil_perc };
     Serial.write((byte*)&data, sizeof(data));
