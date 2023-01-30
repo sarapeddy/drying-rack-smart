5981fae7895a74b0130a37966126ecfc84646556
Cosimo <64689357+cosimop2000@users.noreply.github.com>
2023-01-30 19:33:27 +0100

    aggiunte alcune query di select
    da verificare il funzionamento con il database reale

--- Flask/Flask.py
+++ Flask/Flask.py
@@ -107,6 +107,94 @@ def receive_json():
     # inserire dati dei sensori nel db
     return request_data
 
+@app.route('/<string:rack_user>')
+def display(user):
+    result = select_sensor_feed(user)
+    return json.loads(result)
+
+def select_sensor_feed(user):
+    # seleziona tutti i sensor feed dell'ultimo ciclo asciugatura di un utente
+    query = f"select * from sensor_feed join drying_cycle on(sensor_feed.cycle_id = drying_cycle.id) " \
+            f"where drying_cycle.id >= all( select drying_cycle.id from drying_cycle join rack_user " \
+            f"on (drying_cycle.user_name = rack_user.user_name) " \
+            f"where rack_user.user_name like '{user}');"
+    cur.execute(query)
+    result = cur.fetchall()
+    return result
+
+def select_last_sensor_feed(user):
+    # seleziona l'ultimo sensor feed dell'ultimo ciclo asciugatura dato un utente
+    query = f"select * from sensor_feed join drying_cycle on(sensor_feed.cycle_id = drying_cycle.id)" \
+            f"where drying_cycle.id >= all(select drying_cycle.id from drying_cycle join rack_user " \
+            f"on(drying_cycle.user_name = rack_user.user_name) " \
+            f"where rack_user.user_name like '{user}') and sensor_feed.id >= all(select sensor_feed.id " \
+            f"from sensor_feed join drying_cycle on(sensor_feed.cycle_id = drying_cycle.id));"
+
+    cur.execute(query)
+    result = cur.fetchall()
+    return result
+
+def select_last_weather_feed(user):
+    # seleziona l'ultimo weather_feed dato un utente
+    query = f"select * from weather_feed join rack_user" \
+            f"on( weather_feed.user_name like rack_user.user_name)" \
+            f"where weather_feed.id >= all(select weather_feed.user_name from weather_feed join rack_user" \
+            f"on( weather_feed.user_name = rack_user.user_name)" \
+            f" where rack_user.user_name like '{user}');"
+    cur.execute(query)
+    result = cur.fetchall()
+    return result
+
+def select_all_cycles(user):
+    # seleziona tutti i cicli conclusi di un utente (utile per fare medie e statistiche)
+    query = f"select * from drying_cycle" \
+            f"where drying_cycle.user_name like '{user}'" \
+            f"and drying_cycle.is_active is false;"
+    cur.execute(query)
+    result = cur.fetchall()
+    return result
+
+def select_closing_time_drying_cycle(user):
+    # seleziona il momento di chiusura dell'ultimo ciclo di asciugatura concluso di un dato utente
+    query = f"select * from sensor_feed s join drying_cycle d" \
+            f"on (s.cycle_id = d.id)" \
+            f"where d.user_name like '{user}'" \
+            f"and d.is_active is false" \
+            f"and d.id >=all	(select d1.id from drying_cycle d1" \
+            f"where d1.user_name like d.user_name" \
+            f"and d1.is_active is false)" \
+            f"and s.id >=all	(select s1.id from sensor_feed s1" \
+            f"where s1.cycle_id = d.id);"
+    cur.execute(query)
+    result = cur.fetchall()
+    return result
+
+def select_start_finish_time(user):
+    # seleziona start_time e finish_time di tutti i cicli asciugatura di un utente
+    query = f"select d.start_time, s.sensor_time as finish_time" \
+            f"from drying_cycle d join sensor_feed s on (d.id = s.cycle_id)" \
+            f"where d.user_name like '{user}'" \
+            f"and is_active is false" \
+            f"and s.id >= all(select s1.id from sensor_feed s1" \
+            f"where s1.cycle_id = s.cycle_id);"
+    cur.execute(query)
+    result = cur.fetchall()
+    return result
+
+def select_state(user):
+    # seleziona lo stato (dentro o fuori) degli stendini degli utenti vicini ad un certo utente(<10km)
+    query = f"select r.* from rack_user r join rack_user r1" \
+            f"where r1.user_name like '{user}'" \
+            f"and r.user_name not like '{user}'" \
+            f"and r.is_active is true" \
+            f"and abs(r.lat-r1.lat)<=0.0753" \
+            f"and abs(r.lon-r1.lon)<=0.0753" \
+            f";"
+    cur.execute(query)
+    result = cur.fetchall()
+    return result
+
+
 if __name__ == '__main__':
     host = '0.0.0.0'
     port = 80
