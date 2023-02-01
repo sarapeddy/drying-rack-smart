a69ad78d02f06c49045db94325ee6c42af3b246a
Cosimo <64689357+cosimop2000@users.noreply.github.com>
2023-02-01 19:38:09 +0100

    classe per la gestione delle registrazioni
    da completare

--- Flask/Login.py
+++ Flask/Login.py
@@ -4,24 +4,40 @@ class Login:
     def __init__(self, cur):
         self.cur = cur
 
+    def lat_lon_control(self, lat, lon):
+        lat = int(lat)
+        lon = int(lon)
+        if (lat < -90) or (lat > 90):
+            return 'Insert a correct latitude'
+        elif (lon < -180) or (lon > 180):
+            return 'Insert a correct longitude'
+
+        return 'ok'
+
+    def check_pwd(self, pwd):
+        if len(pwd) < 8:
+            return False
+        return True
+
     def check_db(self, username, password):
         if username is None or password is None:
             return 'Please insert a correct user and a correct password'
+
+        if not self.check_pwd(password):
+            return 'Please insert a correct password'
+
         # seleziona user e pin
-        query = f"select user_name, pin " \
-                f"from rack_user " \
-                f"where user_name like " \
-                f"'{username}' ;"
+        query = f"select user_name " \
+                f"from rack_user "
         self.cur.execute(query)
         result = self.cur.fetchall()
+
+        print(result)
 
         if not result:
             return 'Please insert a correct user and a correct password'
 
-        current_user = result[0][0]
-        current_pin = result[0][1]
+        if username in result:
+            return 'Username already in use'
 
-        if username == current_user and password == current_pin:
-            return current_user
-
-        return 'Please insert a correct user and a correct password'
+        return 'ok'
