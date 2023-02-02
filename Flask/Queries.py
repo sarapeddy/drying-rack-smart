723f572050c162214f92f3d8dd072a55125901b4
Bucci23 <davidebuccioo@gmail.com>
2023-02-02 12:11:31 +0100

    new queries
    moved queries

---
+++ Flask/Queries.py
@@ -0,0 +1,80 @@
+import mysql.connector
+def select_start_finish_time_user(user, cur):
+    # seleziona start_time e finish_time di tutti i cicli asciugatura di un utente
+    query = f"select d.start_time, s.sensor_time as finish_time " \
+            f"from drying_cycle d join sensor_feed s on (d.id = s.cycle_id) " \
+            f"where d.user_name like '{user}' " \
+            f"and is_active is false " \
+            f"and s.id >= all(select s1.id from sensor_feed s1 " \
+            f"where s1.cycle_id = s.cycle_id);"
+    cur.execute(query)
+    result = cur.fetchall()
+    return result
+
+def select_start_finish_time(cur):
+    # seleziona start_time e finish_time di tutti i cicli asciugatura di un utente
+    query = f"select d.start_time, s.sensor_time as finish_time " \
+            f"from drying_cycle d join sensor_feed s on (d.id = s.cycle_id) " \
+            f"where is_active is false " \
+            f"and s.id >= all(select s1.id from sensor_feed s1 " \
+            f"where s1.cycle_id = s.cycle_id);"
+    cur.execute(query)
+    result = cur.fetchall()
+    return result
+
+def select_start_finish_time_hum(cur):
+    #seleziona start_time, finish_time, umidità iniziale ed umidità finale di tutti i cicli di asciugatura
+    query = "select d.start_time, s.sensor_time as finish_time, s2.cloth_humidity as start_hum, s.cloth_humidity as finish_hum, d.id \
+            from drying_cycle d join sensor_feed s on (d.id = s.cycle_id) \
+            join sensor_feed s2 on (d.id = s2.cycle_id) \
+            and is_active is false \
+            and s.id >= all(select s1.id from sensor_feed s1 \
+				            where s1.cycle_id = s.cycle_id) \
+            and s2.id <= all(select s1.id from sensor_feed s1 \
+				            where s1.cycle_id = s.cycle_id);"
+    cur.execute(query)
+    result = cur.fetchall()
+    return result
+
+def select_start_finish_time_hum_user(user, cur):
+    #seleziona start_time, finish_time, umidità iniziale ed umidità finale di tutti i cicli di asciugatura di un dato utente
+    query = f"select d.start_time, s.sensor_time as finish_time, s2.cloth_humidity as start_hum, s.cloth_humidity as finish_hum, d.id \
+            from drying_cycle d join sensor_feed s on (d.id = s.cycle_id) \
+            join sensor_feed s2 on (d.id = s2.cycle_id) \
+            where d.user_name like {user} \
+            and is_active is false \
+            and s.id >= all(select s1.id from sensor_feed s1 \
+				            where s1.cycle_id = s.cycle_id) \
+            and s2.id <= all(select s1.id from sensor_feed s1 \
+				            where s1.cycle_id = s.cycle_id);"
+    cur.execute(query)
+    result = cur.fetchall()
+    return result
+
+def select_avg_tmp_user(user, cur):
+    #seleziona le temperature medie di tutti i cicli di asciugatura di un dato utente
+    query = f'select avg(s.air_temperature) as avg_temperature, d.id from  sensor_feed s join drying_cycle d \
+            on (d.id = s.cycle_id) \
+            where d.user_name like {user} \
+            group by(s.cycle_id); '
+    cur.execute(query)
+    result = cur.fetchall()
+    return result
+
+def select_avg_tmp(cur):
+    #seleziona le temperature medie di tutti i cicli di asciugatura di un dato utente
+    query = 'select avg(s.air_temperature) as avg_temperature, s.cycle_id from  sensor_feed s \
+            group by(s.cycle_id); '
+    cur.execute(query)
+    result = cur.fetchall()
+    return result
+
+def select_total_cycles_last_month(cur):
+    #seleziona il numero di cicli effettuati da ciascun utente nell'ultimo mese
+    query = f'select count(distinct d.id) as total_cycles, d.user_name from drying_cycle d \
+            where timestampdiff(day, d.start_time, now()) < 30 \
+            group by d.user_name \
+            order by 1 desc;'
+    cur.execute(query)
+    result = cur.fetchall()
+    return result
--- Flask/Stats.py
+++ Flask/Stats.py
@@ -1,6 +1,7 @@
 from datetime import datetime
 import mysql.connector
 from configparser import ConfigParser
+import Queries
 '''
 config = ConfigParser()
 config.read('config.ini')
@@ -9,78 +10,7 @@ password = config.get('Database', 'password')
 host = config.get('Database', 'host')
 database = config.get('Database', 'database')
 raise_on = bool(config.get('Database', 'raise_on_warnings'))
-'''
 
-def select_start_finish_time_user(user, cur):
-    # seleziona start_time e finish_time di tutti i cicli asciugatura di un utente
-    query = f"select d.start_time, s.sensor_time as finish_time " \
-            f"from drying_cycle d join sensor_feed s on (d.id = s.cycle_id) " \
-            f"where d.user_name like '{user}' " \
-            f"and is_active is false " \
-            f"and s.id >= all(select s1.id from sensor_feed s1 " \
-            f"where s1.cycle_id = s.cycle_id);"
-    cur.execute(query)
-    result = cur.fetchall()
-    return result
-
-def select_start_finish_time(cur):
-    # seleziona start_time e finish_time di tutti i cicli asciugatura di un utente
-    query = f"select d.start_time, s.sensor_time as finish_time " \
-            f"from drying_cycle d join sensor_feed s on (d.id = s.cycle_id) " \
-            f"where is_active is false " \
-            f"and s.id >= all(select s1.id from sensor_feed s1 " \
-            f"where s1.cycle_id = s.cycle_id);"
-    cur.execute(query)
-    result = cur.fetchall()
-    return result
-
-def select_start_finish_time_hum(cur):
-    #seleziona start_time, finish_time, umidità iniziale ed umidità finale di tutti i cicli di asciugatura
-    query = "select d.start_time, s.sensor_time as finish_time, s2.cloth_humidity as start_hum, s.cloth_humidity as finish_hum, d.id \
-            from drying_cycle d join sensor_feed s on (d.id = s.cycle_id) \
-            join sensor_feed s2 on (d.id = s2.cycle_id) \
-            and is_active is false \
-            and s.id >= all(select s1.id from sensor_feed s1 \
-				            where s1.cycle_id = s.cycle_id) \
-            and s2.id <= all(select s1.id from sensor_feed s1 \
-				            where s1.cycle_id = s.cycle_id);"
-    cur.execute(query)
-    result = cur.fetchall()
-    return result
-
-def select_start_finish_time_hum_user(user, cur):
-    #seleziona start_time, finish_time, umidità iniziale ed umidità finale di tutti i cicli di asciugatura di un dato utente
-    query = f"select d.start_time, s.sensor_time as finish_time, s2.cloth_humidity as start_hum, s.cloth_humidity as finish_hum, d.id \
-            from drying_cycle d join sensor_feed s on (d.id = s.cycle_id) \
-            join sensor_feed s2 on (d.id = s2.cycle_id) \
-            where d.user_name like {user} \
-            and is_active is false \
-            and s.id >= all(select s1.id from sensor_feed s1 \
-				            where s1.cycle_id = s.cycle_id) \
-            and s2.id <= all(select s1.id from sensor_feed s1 \
-				            where s1.cycle_id = s.cycle_id);"
-    cur.execute(query)
-    result = cur.fetchall()
-    return result
-
-def select_avg_tmp_user(user, cur):
-    #seleziona le temperature medie di tutti i cicli di asciugatura di un dato utente
-    query = f'select avg(s.air_temperature) as avg_temperature, d.id from  sensor_feed s join drying_cycle d \
-            on (d.id = s.cycle_id) \
-            where d.user_name like {user} \
-            group by(s.cycle_id); '
-    cur.execute(query)
-    result = cur.fetchall()
-    return result
-
-def select_avg_tmp(cur):
-    #seleziona le temperature medie di tutti i cicli di asciugatura di un dato utente
-    query = 'select avg(s.air_temperature) as avg_temperature, s.cycle_id from  sensor_feed s \
-            group by(s.cycle_id); '
-    cur.execute(query)
-    result = cur.fetchall()
-    return result
-'''
 def testdb():
     global cnx, cur
     try:
@@ -103,9 +33,9 @@ class Statistics:
     def get_mean_cycle_time_user(self, user = None):
         #Ritorna la durata media in secondi dei cicli di asciugatura di un utente (se fornito come argomento) o di tutti (se non si forniscono argomenti)
         if user == None:
-            result = select_start_finish_time(self.cur)
+            result = Queries.select_start_finish_time(self.cur)
         else:
-            result = select_start_finish_time_user(user, self.cur)
+            result = Queries.select_start_finish_time_user(user, self.cur)
         datediff = 0
         k = 0
         print(result)
@@ -117,9 +47,9 @@ class Statistics:
         #Ritorna la durata media in secondi dei cicli di asciugatura, normalizzata alla differenza fra umidità iniziale e finale del vestito
         # di un utente (se fornito come argomento) o di tutti (se non si forniscono argomenti)
         if user == None:
-            result = select_start_finish_time_hum(self.cur)
+            result = Queries.select_start_finish_time_hum(self.cur)
         else:
-            result = select_start_finish_time_hum_user(user, self.cur)
+            result = Queries.select_start_finish_time_hum_user(user, self.cur)
         datediff = 0
         k = 0
         print(result)
@@ -130,11 +60,11 @@ class Statistics:
     def get_normalized_cycle_time_per_temp(self, user = None):
         #ritorna la durata media in secondi dei cicli di asciugatura divisi per temperatura: temp>25, 15<temp<25, temp<15 in una list
         if user == None:
-            result = select_avg_tmp(self.cur)
-            result2 = select_start_finish_time_hum(self.cur)
+            result = Queries.select_avg_tmp(self.cur)
+            result2 = Queries.select_start_finish_time_hum(self.cur)
         else:
-            result = select_avg_tmp_user(self.cur)
-            result2 = select_start_finish_time_hum_user(user, self.cur)  
+            result = Queries.select_avg_tmp_user(self.cur)
+            result2 = Queries.select_start_finish_time_hum_user(user, self.cur)  
         datediff = [0, 0, 0]
         k = 0
         for i in result:
@@ -147,9 +77,10 @@ class Statistics:
                     else:
                         datediff[1] = datediff[1] +((j[1] - j[0]).total_seconds())*100/(j[2] - j[3])
         return datediff
-    def get_n_cycles_user(self, user):
-        pass
-'''
+    def get_total_cycles_user(self):
+        result = Queries.select_tot
+
+    '''
 if __name__ == '__main__':
     testdb()
     s = Statistics(cur)
