40ab095254c0abb0acfff716103ca8fe68b6a42c
Bucci23 <davidebuccioo@gmail.com>
2023-02-03 11:02:11 +0100

    Logout e messaggi ricorrenti
    Implementato il comando /logout ed un primo esempio di messaggi ricorrenti a tutti gli utenti loggati.

--- telegram/telebot.py
+++ telegram/telebot.py
@@ -1,9 +1,8 @@
-
 import logging
-from turtle import position
 from telegram import Update
 from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler, MessageHandler, ContextTypes, filters
 import requests
+from telegram.ext import ContextTypes, Application
 
 BOTKEY = '6152911022:AAHcG-1rkKjBmuv_dZw7iXGrg8jGLbPempM'
 API_LOCATION = 'http://localhost:80'
@@ -27,17 +26,18 @@ class user:
         self.longitude = -1
         self.status = UNLOGGED
 
-def check_username(username):
-    return True
 def check_credentials(username, password):
     dictionary = {}
     dictionary['username'] = username
     dictionary['password'] = password
-    response = requests.post(f'{API_LOCATION}/credentials', json = dictionary)
+    try:
+        response = requests.post(f'{API_LOCATION}/credentials', json = dictionary)
+    except ConnectionError:
+        return UNLOGGED, 'Connection Error: api probably offline, please retry later!'
     if 'Login' in response.text:
-        return True
+        return LOGGED, f'Welcome {username}, you successfully logged in!'
     else:
-        return False
+        return UNLOGGED, 'Username and or password incorrect, /login to try again or /register if you don\'t have an account!'
 
 def insert_new_user(username,lat, lon, password):
     dictionary = {}
@@ -45,10 +45,17 @@ def insert_new_user(username,lat, lon, password):
     dictionary['password'] = password
     dictionary['latitude'] = lat
     dictionary['longitude'] = lon
-    response = requests.post(f'{API_LOCATION}/registration-bot', json = dictionary)
+    try:
+        response = requests.post(f'{API_LOCATION}/registration-bot', json = dictionary)
+    except ConnectionError:
+        return UNLOGGED, 'Connection Error: api probably offline, please retry later!'
     print(response.text)
-    test = 'signed up' in response.text
-    return test
+    if 'signed up' in response.text:
+        return LOGGED, f'Welcome {username}! You are now part of the stendAPP community'
+    if 'password' in response.text:
+        return NEED_PASSWORD_REG, 'Sorry, the password was not suitable: try again (at least 8 characters)'
+    if 'Username' in response.text:
+        return UNLOGGED, 'The username was alredy taken, /login if that is you, or /register to try a new one'
 
 def parse_coordinates(text):
     coord = text.split(',')
@@ -83,6 +90,7 @@ async def register(update: Update, context: ContextTypes.DEFAULT_TYPE):
         await context.bot.send_message(chat_id=update.effective_chat.id, text="You are alredy logged in, to log out type /logout")
     else:
         await context.bot.send_message(chat_id=update.effective_chat.id, text="You were logging, finish that procedure before trying something else!")
+
 async def message_manager(update: Update, context: ContextTypes.DEFAULT_TYPE):
     c_id = update.effective_chat.id
     message = update.message.text
@@ -91,41 +99,21 @@ async def message_manager(update: Update, context: ContextTypes.DEFAULT_TYPE):
     if user_data[c_id].status == UNLOGGED:
         await context.bot.send_message(chat_id=update.effective_chat.id, text="Use /login or /register to join the stendAPP community")
     elif user_data[c_id].status == NEED_USERNAME_REG:
-        if not check_username(message):
-            await context.bot.send_message(chat_id=update.effective_chat.id, text="Sorry, username alredy taken, please retry!")
-            return
         user_data[c_id].status = NEED_POSITION_REG
         user_data[c_id].username = message
         await context.bot.send_message(chat_id=update.effective_chat.id, text=f'Hello, {message}! Now please send your position!')
     elif user_data[c_id].status == NEED_POSITION_REG:
-        '''
-        lat, lon = parse_coordinates(message)
-        if lat == -1 or lon == -1: 
-            await context.bot.send_message(chat_id=update.effective_chat.id, text=f'Sorry, {message}! you have inserted invalid coordinates, try again!')
-            return
-        user_data[c_id].latitude = lat
-        user_data[c_id].longitude = lon
-        user_data[c_id].status = NEED_PASSWORD_REG
-        '''
         await context.bot.send_message(chat_id=update.effective_chat.id, text=f'Again, please send your position!')
     elif user_data[c_id].status == NEED_PASSWORD_REG:
-        if insert_new_user(user_data[c_id].username, user_data[c_id].latitude, user_data[c_id].longitude, message) is True:
-            user_data[c_id].status = LOGGED
-            await context.bot.send_message(chat_id=update.effective_chat.id, text=f'Hello, {user_data[c_id].username}! You are now part of the StendAPP community!')
-        else:
-            await context.bot.send_message(chat_id=update.effective_chat.id, text='Sorry, something went wrong! try again /login or /register')
-            user_data[c_id].status = UNLOGGED
+        user_data[c_id].status, response= insert_new_user(user_data[c_id].username, user_data[c_id].latitude, user_data[c_id].longitude, message)
+        await context.bot.send_message(chat_id=update.effective_chat.id, text=response)       
     elif user_data[c_id].status == NEED_USERNAME_LOG:
         user_data[c_id].status = NEED_PASSWORD_LOG
         user_data[c_id].username = message
         await context.bot.send_message(chat_id=update.effective_chat.id, text=f'Hello, {message}! Now please insert your password!')
     elif user_data[c_id].status == NEED_PASSWORD_LOG:
-        if check_credentials(user_data[c_id].username, message) is True:
-            user_data[c_id].status = LOGGED
-            await context.bot.send_message(chat_id=update.effective_chat.id, text=f'Hello, {user_data[c_id].username}! You are successfully logged in!')
-        else:
-            user_data[c_id].status = UNLOGGED
-            await context.bot.send_message(chat_id=update.effective_chat.id, text=f'Sorry, Username or password incorrect, try again /login or /register')
+        user_data[c_id].status, response = check_credentials(user_data[c_id].username, message)
+        await context.bot.send_message(chat_id=update.effective_chat.id, text=response)
             
 async def login(update: Update, context: ContextTypes.DEFAULT_TYPE):
     c_id = update.effective_chat.id
@@ -143,6 +131,16 @@ async def login(update: Update, context: ContextTypes.DEFAULT_TYPE):
     else:
         await context.bot.send_message(chat_id=update.effective_chat.id, text="You were registering, finish that procedure before trying something else!")
 
+async def logout(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    c_id = update.effective_chat.id
+    if not c_id in user_data.keys():
+        user_data[c_id] = user()
+    if user_data[c_id].status != LOGGED:
+        await context.bot.send_message(chat_id=update.effective_chat.id, text="You don't need to log out, you are not even logged in!")
+    else:
+        user_data[c_id].status = UNLOGGED
+        await context.bot.send_message(chat_id=update.effective_chat.id, text="You are now logged out, use /login or /register")
+
 async def position_manager(update: Update, context: ContextTypes.DEFAULT_TYPE):
     c_id = c_id = update.effective_chat.id
     if user_data[c_id].status == NEED_POSITION_REG:
@@ -154,13 +152,20 @@ async def position_manager(update: Update, context: ContextTypes.DEFAULT_TYPE):
     else:
         await context.bot.send_message(chat_id=update.effective_chat.id, text="We didn't need that position!")
 
+async def callback_minute(context: ContextTypes.DEFAULT_TYPE):
+    for i in user_data.keys():
+        if user_data[i].status == LOGGED:
+            await context.bot.send_message(chat_id=i, text='Hello!')
+
 if __name__ == '__main__':
     application = ApplicationBuilder().token(BOTKEY).build()
-    
     start_handler = CommandHandler('start', start)
     register_handler = CommandHandler('register', register)
     login_handler = CommandHandler('login', login)
     msg_handler = MessageHandler(filters.TEXT & (~filters.COMMAND), message_manager)
     pos_handler = MessageHandler(filters.LOCATION, position_manager)
-    application.add_handlers((start_handler, register_handler,pos_handler, msg_handler, login_handler))
+    logout_handler = CommandHandler('logout', logout)
+    application.add_handlers((start_handler, register_handler,pos_handler, msg_handler, login_handler, logout_handler))
+    job_queue = application.job_queue
+    job_minute = job_queue.run_repeating(callback_minute, interval=120, first=30)
     application.run_polling()
